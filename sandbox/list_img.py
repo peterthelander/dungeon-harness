3e0413af6e76fb52c675a9447a9464bbb9a1237f
Peter Thelander <peter.thelander@gmail.com>
2026-06-03 19:36:32 -0400

    clean up sandbox

--- sandbox/list_img.py
+++
@@ -1,6 +0,0 @@
-import google.genai as genai
-
-client = genai.Client()
-for model in client.models.list():
-    if 'generate_images' in str(model.supported_actions) or 'predict' in str(model.supported_actions) or 'generate' in model.name:
-        print(model.name)
--- sandbox/test_upload.py
+++
@@ -1,8 +0,0 @@
-import google.genai as genai
-import time
-client = genai.Client()
-with open("test.txt", "w") as f: f.write("Hello")
-f = client.files.upload(file="test.txt")
-print(f.state)
-print(dir(f.state))
-print(f.state.name)
